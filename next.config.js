e55f38908f3e322e473a7484b86f8366a67ad63e
Long Hoang <hoangflong2012@gmail.com>
2025-08-16 22:10:34 +0700

    little update

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  compress: true,
   images: {
     unoptimized: true
+  },
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      config.optimization.splitChunks.chunks = 'all';
+    }
+    return config;
   }
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,32 @@ module.exports = {
   content: [
     './src/**/*.{js,jsx,ts,tsx}',
     './pages/**/*.{js,jsx,ts,tsx}',
-    './components/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {},
   },
   plugins: [],
+  corePlugins: {
+    // Disable unused plugins for performance
+    backdropBlur: false,
+    backdropBrightness: false,
+    backdropContrast: false,
+    backdropFilter: false,
+    backdropGrayscale: false,
+    backdropHueRotate: false,
+    backdropInvert: false,
+    backdropOpacity: false,
+    backdropSaturate: false,
+    backdropSepia: false,
+    blur: false,
+    brightness: false,
+    contrast: false,
+    dropShadow: false,
+    filter: false,
+    grayscale: false,
+    hueRotate: false,
+    invert: false,
+    saturate: false,
+    sepia: false,
+  }
 }
